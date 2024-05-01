50339fac963c54b0d80902eb1008894bff7c2bf0
umid0202 <umidjonshoniyozov@gmail.com>
2024-05-01 10:39:41 +0500

    brokers

---
+++ sheenam/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,21 @@
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace sheenam.Brokers.Storages;
+
+public class StorageBroker : EFxceptionsContext
+{
+  private readonly IConfiguration configuration;
+
+  protected StorageBroker(IConfiguration configuration)
+  {
+    this.configuration = configuration;
+    this.Database
+  }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    string connectionString = configuration.GetConnectionString(name: "DefaultConnection");
+    // optionsBuilder
+  }
+}
---
+++ sheenam/Model/Foundations/Guests/GenderType.cs
@@ -0,0 +1,7 @@
+namespace sheenam.Model.Foundations.Guests;
+
+public enum GenderType
+{
+  Male,
+  Female,
+}
